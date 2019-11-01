0e40a494247bec05b19d9c1149f19411ed818023
Elliot Saba <staticfloat@gmail.com>
2019-10-31 21:14:27 -0400

    z3_jll build 4.8.6+0

---
+++ src/z3_jll.jl
@@ -0,0 +1,31 @@
+module z3_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("1bc4e1ec-7839-5212-8f2f-0d16b7bd09bc"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "z3", artifacts_toml) for e in artifacts["z3"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load z3; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module z3_jll
