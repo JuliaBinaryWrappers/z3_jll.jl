b432659acc9a1a36bc3a9cb786a033e14f669631
jlbuild <juliabuildbot@gmail.com>
2026-02-09 15:07:50 +0000

    z3_jll build 4.15.5+0

--- src/z3_jll.jl
+++ src/z3_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("z3")
-JLLWrappers.@generate_main_file("z3", UUID("1bc4e1ec-7839-5212-8f2f-0d16b7bd09bc"))
+JLLWrappers.@generate_main_file("z3", Base.UUID("1bc4e1ec-7839-5212-8f2f-0d16b7bd09bc"))
 end  # module z3_jll
